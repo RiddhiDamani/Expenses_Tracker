385b5efbdffccefb707c0b6c3d3652e8c1eae77d
Riddhi Damani <riddhidamani90@gmail.com>
2021-11-02 23:56:05 -0500

    Added NewExpense Form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,15 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
+  // first var i.e. title is the pointer to the manaaged variable i.e. props.title - First element is current state value
+  // second var is a function which we can later call to set a new title for instance.
   const [title, setTitle] = useState(props.title);
-  let title = props.title;
+  //let title = props.title;
 
   const clickHandler = () => {
-    title = "Updated!";
-    console.log("Clicked!!!");
+    //title = "Updated!";
+    setTitle("Updated!");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
