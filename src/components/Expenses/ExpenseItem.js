2d0e7e9410020a0ccc0ee10f53e95927a4cbc90a
Riddhi Damani <riddhidamani90@gmail.com>
2021-11-03 12:56:46 -0500

    Adding Year Filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
+import React from "react";
 
 const ExpenseItem = (props) => {
-  // first var i.e. title is the pointer to the manaaged variable i.e. props.title - First element is current state value
-  // second var is a function which we can later call to set a new title for instance.
-  const [title, setTitle] = useState(props.title);
-  //let title = props.title;
-
-  const clickHandler = () => {
-    //title = "Updated!";
-    setTitle("Updated!");
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
